5973fac76c9e2cd243f4d085df672bfebf47680d
Carl Furrow <me@carlfurrow.com>
2016-09-05 17:01:33 -0400

    Infinite waves!

--- wave.js
+++ wave.js
@@ -8,28 +8,29 @@ Wave.Game.prototype = {
     this.numWaves = 10;
     this.waves = null;
     this.boat = null;
+    this.lastWaveX = 0;
+    this.WAVE_LENGTH = 160;
   },
 
   preload: function(){
-    this.game.load.spritesheet('wave', 'wave.png', 160, 160);
+    this.game.load.spritesheet('wave', 'wave.png', this.WAVE_LENGTH, this.WAVE_LENGTH);
     this.game.load.image('boat', 'boat.png');
   },
 
   create: function(){
-    var length = this.waveTotalLength / this.numWaves;
     var x, y, wave;
     this.game.world.setBounds(0, 0, 3200, 600);
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
     this.waves = this.game.add.group();
-    this.waves.x = -320;
+    this.waves.x = -this.WAVE_LENGTH*2;
     this.waves.y = this.game.world.height - 50;
     this.waves.enableBody = true;
     this.waves.physicsBodyType = Phaser.Physics.ARCADE;
 
     for (var i = 0; i < this.numWaves; i++)
     {
-      x = i*length;
+      x = i*this.WAVE_LENGTH;
       y = 0;
       wave = this.game.add.sprite(x, y, 'wave', this.game.rnd.between(0,1));
       wave.anchor.set(0.5,0.5);
@@ -37,6 +38,7 @@ Wave.Game.prototype = {
       wave.body.setCircle(this.game.rnd.between(80,140));
       wave.body.offset.set(0, 50);
       wave.body.immovable = true;
+      this.lastWaveX = wave.x;
     }
 
     this.boat = this.game.add.sprite(0, 0, 'boat');
@@ -50,12 +52,23 @@ Wave.Game.prototype = {
     this.game.physics.arcade.collide(this.boat, this.waves);
     this.count += 0.1;
     this.boat.body.velocity.x = 90; // Constantly move boat to the right
+    this.game.debug.text("Camera "+this.game.camera.x, 0, 10);
 
     this.waves.forEach(function(currentWave){
       var i = this.waves.getChildIndex(currentWave);
-      var x = i * 0.9 + this.count;
       var amp = 5;
-      currentWave.y = Math.sin(x) * amp;
+      var x = i * 0.9 + this.count;
+      var y = Math.sin(x) * amp;
+      var newX;
+      currentWave.y = y;
+
+      this.game.debug.text("Wave["+i+"]: (" + currentWave.x + ","+ currentWave.y+")", 10, 11*i+20)
+
+      // check if the current wave's right edge (in world coords) is less than the camera's left edge
+      if((currentWave.world.x + currentWave.offsetX) < this.game.camera.x) {
+        newX = this.lastWaveX+this.WAVE_LENGTH;
+        currentWave.x = this.lastWaveX = newX;
+      }
     }, this);
   },
 
